0e658e6e9fa0bfdff28d4224903a1b2dcc2f0289
useredsa <emilio.dominguezs@um.es>
2021-12-26 18:53:16 +0100

    Fixed blank spaces

--- kernel/kalloc.h
+++ kernel/kalloc.h
@@ -8,22 +8,22 @@
  */
 void kinit();
 
-/* 
+/*
  * Returns a free physical page.
  * Returns 0 if the memory cannot be allocated.
  */
 uint64 kalloc();
 
 /*
- * Decrement number of references of a physical page and if the number 
+ * Decrement number of references of a physical page and if the number
  * of references reaches 0 then mark it as free.
  */
-void kdecref(uint64 pa); 
+void kdecref(uint64 pa);
 
 /*
  * Increment number of references of a physical page.
  */
-void kincref(uint64 pa); 
+void kincref(uint64 pa);
 
 /*
  * Check if page is uniquely referenced.
